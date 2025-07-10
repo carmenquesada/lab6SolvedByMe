185ed1c3e44950134bd9273c50a2cb4fcaad297c
CARMEN QUESADA <carmenquesadarguez@gmail.com>
2025-07-10 15:43:03 +0200

    ImagePicker

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -8,7 +8,7 @@ import { create, getRestaurantCategories } from '../../api/RestaurantEndpoints'
 import InputItem from '../../components/InputItem'
 import TextRegular from '../../components/TextRegular'
 import * as GlobalStyles from '../../styles/GlobalStyles'
-import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg' // Agregar un imagePicker (selector de imagen) al form
 import restaurantBackground from '../../../assets/restaurantBackground.jpeg'
 import { showMessage } from 'react-native-flash-message'
 import { ErrorMessage, Formik } from 'formik'
@@ -17,6 +17,31 @@ import TextError from '../../components/TextError'
 export default function CreateRestaurantScreen () {
   // Definir valores iniciales del form:
   const initialRestaurantValues = { name: null, description: null, address: null, postalCode: null, url: null, shippingCosts: null, email: null, phone: null, restaurantCategoryId: null}
+  // ImagePicker: pedir permisos de acceso a la galería
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ExpoImagePicker.requestMediaLibraryPermissionsAsync()
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      }
+    })()
+  }, [])
+  // ImagePicker: definir la función pickImage
+  const pickImage = async (onSuccess) => {
+    const result = await ExpoImagePicker.launchImageLibraryAsync({
+      mediaTypes: ExpoImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    })
+    if (!result.canceled) {
+      if (onSuccess) {
+        onSuccess(result)
+      }
+    }
+  }
   return (
     // Envolver el contenido con <Formik>:
     <Formik initialValues={initialRestaurantValues}>
@@ -57,6 +82,18 @@ export default function CreateRestaurantScreen () {
         name='sampleInput'
         label='Sample input'
       />
+      <Pressable onPress={() => // ImagePicker: new pressable element. Once pressed, select an image
+        pickImage(
+          async result => {
+            await setFieldValue('logo', result)
+          }
+        )
+      }
+        style={styles.imagePicker}
+      >
+        <TextRegular>Logo: </TextRegular>
+        <Image style={styles.image} source={values.logo ? { uri: values.logo.assets[0].uri } : restaurantLogo} />
+      </Pressable>
       <Pressable
         onPress={() => console.log('Button pressed')
         }
@@ -93,5 +130,19 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: GlobalStyles.brandSecondary,
     textAlign: 'center'
+  },
+  // Styling additions
+  imagePicker: {
+    height: 40,
+    paddingLeft: 10,
+    marginTop: 20,
+    marginBottom: 80
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 5
   }
 })
